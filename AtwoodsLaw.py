54437f64a0a00143fa37bf998bc702e99659d017
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-10-31 19:43:13 +0200

    Create AtwoodsLaw.py

---
+++ AtwoodsLaw.py
@@ -0,0 +1,12 @@
+import time
+
+
+t1 = time.time()
+
+for _ in range(2000):
+    print("Any application that can be written in JavaScript, will eventually be written in JavaScript.")
+
+
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
